bc62ae27b1ab870218eb394b90f140a3daf5b8ff
kimgazii <gy0320ya@naver.com>
2024-11-19 00:03:25 +0900

    책장  페이지 추가

---
+++ lib/bookShelf.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'allBooks.dart';
+import 'myPage.dart';
+import 'myHome.dart';
+
+class BookshelfPage extends StatefulWidget {
+  @override
+  _BookshelfPageState createState() => _BookshelfPageState();
+}
+
+class _BookshelfPageState extends State<BookshelfPage> {
+  int _selectedIndex = 1;
+  final PageController _pageController = PageController(viewportFraction: 1.0);
+
+  // 탭을 눌렀을 때 페이지 변경
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    if (index == 0) {
+      // 홈 탭 클릭 시 MyHomePage로 이동
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage(username: 'username')),
+      );
+    } else if (index == 1) {
+      // 책장 탭 클릭 시 BookshelfPage로 이동
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => BookshelfPage()),
+      );
+    } else if (index == 2) {
+      // 도서 탭 클릭 시 AllBooksPage로 이동
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => AllBooksPage()),
+      );
+    } else if (index == 3) {
+      // 마이페이지 탭 클릭 시 MyPage로 이동
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MyPage()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bookshelf'),
+        backgroundColor: Color.fromARGB(255, 126, 113, 159), // 앱바 색상
+      ),
+      body: Container(
+        color: Colors.white, // 배경색 흰색으로 설정
+        child: Center(
+          child: Text(
+            '책장 페이지입니다.', // 책장 페이지 메시지
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.folder_open),
+            label: '책장',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.format_list_bulleted),
+            label: '도서',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: '마이페이지',
+          ),
+        ],
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
+      ),
+    );
+  }
+}
