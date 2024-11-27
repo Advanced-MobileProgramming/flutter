d3c85236186714ad95212676edf745945e1d4fe1
Sun Dayeon <yeoniv7@hansung.ac.kr>
2024-11-28 00:19:01 +0900

    allBooks - db 연결 완료 상세 정보 확인 가능

--- lib/allBooks.dart
+++ lib/allBooks.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:soobook/bookShelf.dart';
 import 'package:soobook/myPage.dart';
@@ -14,25 +15,41 @@ class AllBooksPage extends StatefulWidget {
 
 class _AllBooksPageState extends State<AllBooksPage> {
   int _selectedIndex = 2;
-  // 책 리스트
-  final List<Map<String, dynamic>> books = List.generate(
-    10,
-    (index) => {
-      "title": "Book $index",
-      "image": 'image/book_image_${index + 1}.jpg', // 실제 책 이미지 경로로 변경
-      "author": "Author $index", // 책 저자
-      "description": "책에 대한 간단한 설명입니다.", // 책 설명
-      "status": index % 2 == 0
-          ? "reading" // 읽는 중
-          : "completed", // 완료
-      "progress": index % 2 == 0 ? 0.3 * (index + 1) % 1 : 1.0, // 읽기 진행 상태
-    },
-  );
-
-  final PageController _pageController =
-      PageController(viewportFraction: 0.5); // viewportFraction을 0.5로 설정
-
+  // 수정된 코드
+  List<Map<String, dynamic>> books = [];
+  bool isLoading = true;
   String searchQuery = '';
+
+  Future<void> fetchBooks() async {
+    final DatabaseReference booksRef = FirebaseDatabase.instance.ref("books");
+
+    try {
+      final DataSnapshot snapshot = await booksRef.get();
+      if (snapshot.exists) {
+        final data = snapshot.value as List<dynamic>;
+        setState(() {
+          books = data.map((book) => Map<String, dynamic>.from(book)).toList();
+          isLoading = false;
+        });
+      } else {
+        setState(() {
+          books = [];
+          isLoading = false;
+        });
+      }
+    } catch (e) {
+      print("Firebase 데이터 가져오기 오류: $e");
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchBooks(); // 책 데이터 가져오기
+  }
 
   // 탭을 눌렀을 때 페이지 변경
   void _onItemTapped(int index) {
@@ -157,12 +174,13 @@ class _AllBooksPageState extends State<AllBooksPage> {
               child: GridView.builder(
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 3, // 3개의 열
-                  crossAxisSpacing: 8, // 열 간 간격
-                  mainAxisSpacing: 8, // 행 간 간격
+                  crossAxisSpacing: 12, // 열 간 간격
+                  mainAxisSpacing: 16, // 행 간 간격
                   childAspectRatio: 0.7, // 아이템의 가로 세로 비율 (이미지 크기 조정)
                 ),
                 itemCount: filteredBooks.length,
                 itemBuilder: (context, index) {
+                  final book = filteredBooks[index]; // 추가
                   return GestureDetector(
                     onTap: () {
                       // 카드를 눌렀을 때 동작
@@ -171,70 +189,93 @@ class _AllBooksPageState extends State<AllBooksPage> {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => UnstoredBookDetail(
-                            title: filteredBooks[index]["title"]!,
-                            image: filteredBooks[index]["image"]!,
-                            author: filteredBooks[index]["author"]!,
-                            description: filteredBooks[index]["description"]!,
-                            status: filteredBooks[index]["status"]!,
-                            progress: filteredBooks[index]["progress"]!,
-                            startDay: '',
-                            endDay: '',
-                            publisher: '한빛미디어',
-                            publishYear: '2023',
-                            publishMonth: '3',
-                          ),
+                          builder: (context) {
+                            final timestamp = book["publication_date"];
+                            String publishYear = "0000";
+                            String publishMonth = "00";
+
+                            if(timestamp != null && timestamp is int) {
+                              final date = DateTime.fromMillisecondsSinceEpoch(timestamp * 1000);
+                              publishYear = date.year.toString();
+                              publishMonth = date.month.toString().padLeft(2, '0');
+                            }
+
+                            return UnstoredBookDetail(
+                              title: book["title"] ?? "제목 없음",
+                              image: book["image_path"] ?? "image/book_image_1.jpg",
+                              author: book["author"] ?? "저자 없음",
+                              description: book["description"] ?? "설명 없음",
+                              status: "",
+                              progress: 0.0,
+                              startDay: "",
+                              endDay: "",
+                              publisher: book["publisher"] ?? "출판사 없음",
+                              publishYear: publishYear,
+                              publishMonth: publishMonth,
+                            // title: filteredBooks[index]["title"]!,
+                            // image: filteredBooks[index]["image"]!,
+                            // author: filteredBooks[index]["author"]!,
+                            // description: filteredBooks[index]["description"]!,
+                            // status: filteredBooks[index]["status"]!,
+                            // progress: filteredBooks[index]["progress"]!,
+                            // startDay: '',
+                            // endDay: '',
+                            // publisher: '한빛미디어',
+                            // publishYear: '2023',
+                            // publishMonth: '3',
+                            );
+                          },
                         ),
                       );
                     },
-                    child: Card(
-                      color: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      elevation: 4,
                       child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.asset(
-                          filteredBooks[index]["image"]!, // 동적으로 이미지 변경
-                          fit: BoxFit.cover, // 이미지를 카드 크기에 맞게 채움
-                        ),
-                      ),
+                            borderRadius: BorderRadius.circular(10), // 모서리를 둥글게
+                            child: Image.asset(
+                              book["image_path"] ?? "image/book_image_1.jpg", // 이미지 경로
+                              fit: BoxFit.cover, // 이미지가 영역을 꽉 채우도록 설정
+                              errorBuilder: (context, error, stackTrace) {
+                                return Icon(
+                                  Icons.image_not_supported,
+                                  size: 50,
+                                  color: Colors.grey,
+                                );
+                              },
+                            ),
+                          ),
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '홈',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.folder_open),
-            label: '책장',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.format_list_bulleted),
-            label: '도서',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: '마이페이지',
-          ),
-        ],
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.black,
-        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
-        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
-      ),
-    );
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: '홈',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.folder_open),
+              label: '책장',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.format_list_bulleted),
+              label: '도서',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              label: '마이페이지',
+            ),
+          ],
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.black,
+          selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+          unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
+        ),
+      );
+    }
   }
-}
